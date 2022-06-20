915a13a51253f342338795dfde852171620e0d88
svttop <106789592+svttop@users.noreply.github.com>
2022-06-20 19:17:38 +1200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -65,7 +65,7 @@ function displayForecast(response) {
       forecastHTML =
         forecastHTML +
         `
-      <div class="col-2">
+      <div class="col">
         <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
         <img
           src="http://openweathermap.org/img/wn/${
